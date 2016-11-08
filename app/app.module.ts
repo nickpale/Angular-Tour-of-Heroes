6be0911c246424919edb146a545c242593701b8f
whiteisnick <whiteisnick@gmail.com>
2016-11-07 22:26:25 -0500

    Set up HTTP in Hero Service

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,19 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
+import { AppRoutingModule }    from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 
-import { AppRoutingModule }    from './app-routing.module';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
