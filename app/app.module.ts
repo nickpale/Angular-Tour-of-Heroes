0207a30f0c90035b7e90e84c748c5d7448181a4d
whiteisnick <whiteisnick@gmail.com>
2016-11-07 23:30:10 -0500

    Added search to dashboard using Observables - Finished Tour of Heroes Tutorial!

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
+import './rxjs-extensions';
 
 import { AppRoutingModule }    from './app-routing.module';
 
@@ -12,6 +13,7 @@ import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent}  from './hero-search.component';
 import { HeroService }         from './hero.service';
 
 
@@ -27,7 +29,8 @@ import { HeroService }         from './hero.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
